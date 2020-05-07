b6852e6adc970aa364b330cafceab7ab460c598d
Jean Souza <jeansouz4@gmail.com>
2020-05-07 19:42:36 -0300

    finalizado estrutura de dados de paginação

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,6 @@
 const currentPage = location.pathname
 const menuItems = document.querySelectorAll("header .links a")
 
-
-
 for (item of menuItems) {
     if (currentPage == includes(item.getAttribute("href"))) {
         item.classList.add("active")
@@ -11,21 +9,35 @@ for (item of menuItems) {
 
 //Paginação
 
-let totalPages = 20
-    selectedPage = 15
-    pages = []
+function pagination(selectedPage, totalPages) {
+    let pages = [],
+        oldPage
 
     for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-       
 
         const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-        const pagesAfterSelectedPage = currentPage <= selectedPage + 2 
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
         const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
 
+        if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
 
-        if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+            if (oldPage && currentPage - oldPage > 2) {
+
+                pages.push("...")
+
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+
+                pages.push(oldPage + 1)
+
+            }
+
             pages.push(currentPage)
+            oldPage = currentPage
         }
     }
 
-    console.log(pages)
+    return pages
+}
+
